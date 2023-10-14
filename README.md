85726fdd56ab8ef8e5a7014e4997d5004a4b6e13
mikecasey12 <ikebudemichael@gmail.com>
2023-10-14 09:45:43 +0100

    webscrape for polling unit latitude and longitude

---
+++ README.md
@@ -0,0 +1,110 @@
+# INEC Polling Unit Latitude and Longitude Scraper
+
+## Overview
+
+This Node.js web scraping script is designed to retrieve the latitude and longitude coordinates for each polling unit in Nigeria from the INEC (Independent National Electoral Commission) Voters Online Portal. It uses the Puppeteer library to automate the web scraping process.
+
+## Prerequisites
+
+Before running the script, ensure you have the following prerequisites installed on your system:
+
+- Node.js: [Install Node.js](https://nodejs.org/)
+- Puppeteer: This is the web scraping library used in the script. You can install it using npm:
+
+```bash
+npm install puppeteer
+```
+
+## Usage
+
+1. **Clone this Git repository to your local machine:**
+
+```bash
+git clone https://github.com/yourusername/inec-polling-unit-scraper.git
+cd inec-polling-unit-scraper
+```
+
+2. **Navigate to the app directory:**
+
+```bash
+cd app
+```
+
+3. **Run the script using Node.js:**
+
+```bash
+node result_scraper
+```
+
+The script will start scraping the INEC Voters Online Portal and collect latitude and longitude data for polling units in Nigeria.
+
+## Scraping a Specific State
+
+To scrape only a specific state change the index of in options to the value of each state as seen below.
+To achieve this use the **scrapper.js**
+
+```bash
+const optionToSelect = await page.$$eval(
+    "#SearchStateId > option",
+    (selectOptions) => {
+      const options = selectOptions.map((selectOption) => ({
+        value: selectOption.value,
+        name: selectOption.textContent,
+      }));
+      const option = options[6]; //index 6 representing BAYELSA
+      return option;
+    }
+  );
+```
+
+State Value
+ABIA | 1
+ADAMAWA | 2
+AKWA IBOM | 3
+ANAMBRA | 4
+BAUCHI | 5
+BAYELSA | 6
+BENUE | 7
+BORNO | 8
+CROSS RIVER | 9
+DELTA | 10
+EBONYI | 11
+EDO | 12
+EKITI | 13
+ENUGU | 14
+FCT | 15
+GOMBE | 16
+IMO | 17
+JIGAWA | 18
+KADUNA | 19
+KANO | 20
+KATSINA | 21
+KEBBI | 22
+KOGI | 23
+KWARA | 24
+LAGOS | 25
+NASARAWA | 26
+NIGER | 27
+OGUN | 28
+ONDO | 29
+OSUN | 30
+OYO | 31
+PLATEAU | 32
+RIVERS | 33
+SOKOTO | 34
+TARABA | 35
+YOBE | 36
+ZAMFARA | 37
+
+## Important Notes
+
+- Please use this script responsibly and ethically.
+
+- The script may take some time to complete, depending on the number of polling units and the performance of the INEC website.
+
+- It's essential to handle the scraped data in compliance with privacy and data protection regulations.
+
+Author
+Michael Ikebude
+Email: ikebudemichael@gmail.com
+GitHub: mikecasey12
---
+++ app/scrapper.js
@@ -0,0 +1,190 @@
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+const url = "https://cvr.inecnigeria.org/pu";
+
+const scrapePollingData = async () => {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  const data = {
+    state: "",
+    lga: "",
+    rga: "",
+    pu: "",
+    lat: "",
+    long: "",
+    url: "",
+  };
+  await page.goto(url);
+
+  //select specific state to scrape
+  const optionToSelect = await page.$$eval(
+    "#SearchStateId > option",
+    (selectOptions) => {
+      const options = selectOptions.map((selectOption) => ({
+        value: selectOption.value,
+        name: selectOption.textContent,
+      }));
+      const option = options[6];
+      return option;
+    }
+  );
+
+  data.state = optionToSelect.name.split("-")[1].trim();
+  await page.select("#SearchStateId", optionToSelect.value);
+
+  //wait for the RLocal Government Area to load on page
+  await page.waitForXPath('//*[@id="SearchLocalGovernmentId"]/option[2]');
+  const lgaOptionsToSelect = await page.$$eval(
+    "#SearchLocalGovernmentId > option",
+    (selectOptions) => {
+      const options = selectOptions.map((selectOption) => {
+        if (selectOption.value === "0") {
+          return { value: null, name: null };
+        }
+        return {
+          value: selectOption.value,
+          name: selectOption.textContent,
+        };
+      });
+      return options;
+    }
+  );
+
+  for (const lgaOptionToSelect of lgaOptionsToSelect) {
+    if (lgaOptionToSelect.value === null) {
+      continue;
+    }
+
+    //add data to object
+    data.lga = lgaOptionToSelect?.name?.split("-")[1].trim();
+
+    //select lga option from dropdown
+    await page.select("#SearchLocalGovernmentId", lgaOptionToSelect.value);
+
+    //wait for the Registration Area to load on page
+    await page.waitForXPath('//*[@id="SearchRegistrationAreaId"]/option[2]');
+
+    //wait for each option to be loaded on the DOM
+    await page.waitForNetworkIdle();
+    const regAreaOptionsToSelect = await page.$$eval(
+      "#SearchRegistrationAreaId > option",
+      (selectOptions) => {
+        const options = selectOptions.map((selectOption, index) => {
+          if (selectOption.value === "0") {
+            return { value: null, name: null };
+          }
+          return {
+            value: selectOption.value,
+            name: selectOption.textContent,
+          };
+        });
+        return options;
+      }
+    );
+    for (const regAreaOptionToSelect of regAreaOptionsToSelect) {
+      if (regAreaOptionToSelect.value === null) {
+        continue;
+      }
+
+      //add data to object
+      data.rga = regAreaOptionToSelect?.name?.split("-")[1].trim();
+
+      //select option from dropdown using the value
+      await page.select(
+        "#SearchRegistrationAreaId",
+        regAreaOptionToSelect.value
+      );
+
+      //wait for the Polling Unit to load on page
+      await page.waitForXPath('//*[@id="SearchPollingUnitId"]/option[2]');
+
+      //wait for option data to be loaded on the DOM
+      await page.waitForNetworkIdle();
+
+      //retrieves all polling unit options
+      const pollingUnitOptionsToSelect = await page.$$eval(
+        "#SearchPollingUnitId > option",
+        (selectOptions) => {
+          const options = selectOptions.map((selectOption, index) => {
+            //skips the first option
+            if (selectOption.value === "0") {
+              return { value: null, name: null };
+            }
+
+            return {
+              value: selectOption.value,
+              name: selectOption.textContent,
+            };
+          });
+          return options;
+        }
+      );
+
+      //looping through polling unit data to get individual options
+      for (const pollingUnitOptionToSelect of pollingUnitOptionsToSelect) {
+        if (pollingUnitOptionToSelect.value === null) {
+          continue;
+        }
+
+        //add data to object
+        data.pu = pollingUnitOptionToSelect?.name?.split("-")[1].trim();
+
+        //select option from dropdown using the value
+        await page.select(
+          "#SearchPollingUnitId",
+          pollingUnitOptionToSelect.value
+        );
+
+        //Click the get direction button and getting the url
+        const [newTarget] = await Promise.all([
+          //resolves promise on by getting the url from the newly opened tab
+          new Promise((resolve, reject) => {
+            browser.once("targetcreated", (target) => {
+              resolve(target);
+            });
+          }),
+
+          //click on button
+          page.click("#SearchIndexForm > div.submit > input"),
+        ]);
+
+        //Getting the url which contains the long and lat
+        const url2 = newTarget.url();
+        data.url = url2;
+
+        //Extracting the longtitude and latitude from the url
+        const newUrl = new URL(url2);
+        const searchParams = newUrl.searchParams;
+
+        const latitude = searchParams.get("q")?.split(",")[0];
+        const longitude = searchParams.get("q")?.split(",")[1];
+        data.lat = latitude;
+        data.long = longitude;
+
+        //get number of currently opened tabs and close the newly opened tab
+        const pages = await browser.pages();
+        const newPage = pages[pages.length - 1];
+        await newPage.close();
+
+        //writing the data into a file
+        fs.appendFile(
+          "pollinglonglat.csv",
+          `${data.state}-${data.lga}-${data.rga}-${data.pu}-${data.lat}-${data.long}-${data.url}\r\n`,
+          (err) => {
+            if (err) {
+              console.log("An error occured");
+            }
+            console.log("File saved successfully");
+          }
+        );
+      }
+    }
+  }
+
+  await browser.close();
+};
+
+//Function Call
+scrapePollingData();
